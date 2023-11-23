e358b658682def1e0729c547639455f41613dfa8
Rajdeep Chakraborty <ratnac1001@gmail.com>
2023-11-24 01:18:08 +0530

    Render songs from Song objects

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use practicals\Song;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -32,3 +34,18 @@ Route::get('/veggies/{veggiename}', function (string $veggiename) {
 /*Route::get('/veggies/{veggiename}', function (string $veggiename) {
     return view('veggies');
 })->whereIn('veggiename', ['baigan', 'gobhi', 'aaloo','bhindi']);*/
+Route::get('/songs', function () {
+    $song1 = new Song("Song Title", "Artist Name", "Pop", 120);
+    $song1->setTitle("Stan");
+    $song1->setArtist("Eminem");
+  
+    $song2 = new Song("Song Title", "Artist Name", "Pop", 120);
+    $song2->setTitle("Nothing Else Matters");
+    $song2->setArtist("Metallica");
+  
+    $song3 = new Song("Song Title", "Artist Name", "Pop", 120);
+    $song3->setTitle("With You");
+    $song3->setArtist("A P Dhillon");
+  
+    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
+  });
